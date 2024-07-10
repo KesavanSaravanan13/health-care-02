9c4a317800b3c2a748c1249127a5db9fcd6b70ae
KesavanSaravanan13 <kesavan.saravanan@concertidc.com>
2024-07-10 20:32:46 +0530

    aligned column

--- src/Component/MainColumn/MainColumn.js
+++ src/Component/MainColumn/MainColumn.js
@@ -2,15 +2,13 @@ import './MainColumn.css';
 import TopRow from '../TopRow/TopRow';
 const MainColumn = () => {
     return (
-        <div className='row m-0 p-0'>
-            <div className="col-12 m-0 p-2 p-md-4 ">
-                <div className="row p-0 m-0 flex-fill d-flex justify-content-end">
-                    <TopRow />
-                </div>
+        <div className="col-11 m-0 p-2 p-md-4 flex-fill">
+            <div className="row p-0 m-0 flex-fill d-flex justify-content-end">
+                <TopRow />
             </div>
         </div>
     );
 };
 
-  
+
 export default MainColumn;
